b8fe399cd25e8279ff689cf5bdd1376b9e578bbf
Andrey <kezin636@gmail.com>
2023-04-13 02:03:54 +0300

    first button working hook boilerplate

--- src/main.cpp
+++ src/main.cpp
@@ -13,15 +13,81 @@
 #include <gd.h>
 
 #include <cpr/cpr.h>
-#include <nlohmann/json.hpp>
+#include "utils/json.hpp"
 
-bool MenuLayer_init(gd::MenuLayer* self) {
-    if (!matdash::orig<&MenuLayer_init>(self)) return false;
+using namespace cocos2d;
 
-    return true;
+gd::FLAlertLayer* openAlert;
+bool shareConfirmation;
+CCNode* maybeShareLayer;
+
+const char* get_node_name(CCNode* node) {
+    return typeid(*node).name() + 6;
 }
+
+void EditLevelLayer_onShare(gd::EditLevelLayer* self, CCObject* sender) {
+    matdash::orig<&EditLevelLayer_onShare>(self, nullptr);
+
+    shareConfirmation = false;
+
+    CCScene* parentScene = CCDirector::sharedDirector()->getRunningScene();
+
+    std::cout << "Cocos scene nodes count" << parentScene->getChildrenCount() << std::endl;
+
+    for (int i = 0; i < parentScene->getChildrenCount(); i++) {
+        CCNode* buffer = (CCNode*)(parentScene->getChildren()->objectAtIndex(i));
+        const char* nodename = get_node_name(buffer);
+
+        std::cout << "And the " << i << " element is " << nodename << std::endl;
+
+        if (*nodename == *(std::string("ShareLevelLayer").c_str())) {
+            std::cout << "Setting... ShareLevelLayer " << std::endl;
+
+            maybeShareLayer = (CCNode*)(parentScene->getChildren()->objectAtIndex(i));
+
+            std::cout << "ShareLevelLayer set!" << std::endl;
+        }
+            
+    }
+    maybeShareLayer->setVisible(false);
+
+    shareConfirmation = true;
+   
+    openAlert = gd::FLAlertLayer::create(self, "Choose", "Share", "Open Collab", "Do you wanna <cg>share level</c> or <cy>open collab?</c>");
+    openAlert->show();
+    
+}
+
+
+void onShowCollab(gd::EditLevelLayer* self) {
+    gd::AchievementNotifier::sharedState()->notifyAchievement("Placeholder", "Collab is opening..", nullptr, false);
+}
+
+
+void EditLevelLayer_FLAlert_Clicked(gd::EditLevelLayer* self, gd::FLAlertLayer* alert, bool openCollabVariant) {
+
+    if (shareConfirmation) {
+        std::cout << "Open collab: " << openCollabVariant << std::endl;
+
+        if (!openCollabVariant) {
+            maybeShareLayer->setVisible(true);
+        }
+        else {
+            maybeShareLayer->removeFromParent();
+            onShowCollab(self);
+        }
+
+        shareConfirmation = false;
+    }
+    
+
+    matdash::orig<&EditLevelLayer_FLAlert_Clicked>(self, alert, openCollabVariant);
+}
+
 
 void mod_main(HMODULE) {
     matdash::create_console();
-    matdash::add_hook<&MenuLayer_init>(gd::base + 0x1907B0);
+
+    matdash::add_hook<&EditLevelLayer_onShare>(gd::base + 0x71BE0);
+    matdash::add_hook<&EditLevelLayer_FLAlert_Clicked>(gd::base + 0x71F80);
 }
