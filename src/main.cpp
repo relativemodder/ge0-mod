51fa38c0087aa58211f8e850b53f657a52248f15
Andrey <kezin636@gmail.com>
2023-04-14 00:40:27 +0300

    clean code with callbacks

--- src/main.cpp
+++ src/main.cpp
@@ -8,21 +8,24 @@ using namespace cocos2d;
 namespace modmain {
 
     void onShowCollab(gd::EditLevelLayer* self) {
-        //gd::AchievementNotifier::sharedState()->notifyAchievement("Placeholder", "Collab is opening..", nullptr, false);
+        gd::AchievementNotifier::sharedState()->notifyAchievement("Placeholder", "Collab is opening..", nullptr, false);
     }
 
 
     void onCreateEditLevelLayer(gd::EditLevelLayer* self, gd::GJGameLevel* level) {
-        //gd::AchievementNotifier::sharedState()->notifyAchievement("EditLevelLayer", (std::string("Created with level: ") + std::string(level->m_sLevelName)).c_str(), nullptr, false);
+        gd::AchievementNotifier::sharedState()->notifyAchievement("EditLevelLayer", (std::string("Created with level: ") + std::string(level->m_sLevelName)).c_str(), nullptr, false);
     }
 }
-
 
 namespace modmainhooks {
 
     gd::FLAlertLayer* openAlert;
     bool shareConfirmation;
     CCNode* maybeShareLayer;
+
+
+    void (*onCreateEditLevelLayer)(gd::EditLevelLayer*, gd::GJGameLevel*);
+    void (*onShowCollab)(gd::EditLevelLayer*);
 
 
     void EditLevelLayer_onShare(gd::EditLevelLayer* self, CCObject* sender) {
@@ -48,7 +51,7 @@ namespace modmainhooks {
             }
             else {
                 maybeShareLayer->removeFromParent();
-                modmain::onShowCollab(self);
+                onShowCollab(self);
             }
 
             shareConfirmation = false;
@@ -58,7 +61,7 @@ namespace modmainhooks {
 
     gd::EditLevelLayer* EditLevelLayer_create(gd::GJGameLevel* level) {
         gd::EditLevelLayer* created_layer = matdash::orig<&EditLevelLayer_create>(level);
-        modmain::onCreateEditLevelLayer(created_layer, level);
+        onCreateEditLevelLayer(created_layer, level);
         return created_layer;
     }
 }
@@ -66,6 +69,9 @@ namespace modmainhooks {
 
 
 void mod_main(HMODULE) {
+    modmainhooks::onCreateEditLevelLayer = modmain::onCreateEditLevelLayer;
+    modmainhooks::onShowCollab = modmain::onShowCollab;
+
     matdash::add_hook<&modmainhooks::EditLevelLayer_create>(gd::base + 0x6F530);
     matdash::add_hook<&modmainhooks::EditLevelLayer_onShare>(gd::base + 0x71BE0);
     matdash::add_hook<&modmainhooks::EditLevelLayer_FLAlert_Clicked>(gd::base + 0x71F80);
